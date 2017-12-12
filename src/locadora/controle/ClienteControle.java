9c117364dc77748ad3c68efef6af44a0cf2698fb
Maycon-Ferreira <32937928+Maycon-Ferreira@users.noreply.github.com>
2017-12-12 19:38:45 -0200

    Implementação do controle do Cliente

--- src/locadora/controle/ClienteControle.java
+++ src/locadora/controle/ClienteControle.java
@@ -1,9 +1,39 @@
 package locadora.controle;
 
+import locadora.Database;
+import locadora.Fabrica;
+import locadora.FabricaCliente;
+import locadora.modelo.Cliente;
+
 /**
  * ClienteControle
  */
-public class ClienteControle implements Controle{
+public class ClienteControle implements Controle {
 
-    
+    private FabricaCliente fabrica;
+
+    public ClienteControle(){
+
+        fabrica = (FabricaCliente) Fabrica.getFabrica("cliente");
+    }
+
+    public boolean adicionarCliente(String nome, String cpf){
+
+        Cliente cliente = (Cliente) fabrica.criaModelo();
+
+        cliente.setCpf(cpf);
+        cliente.setNome(nome);
+
+        return Database.getInstance().adicionar(cliente);
+    }
+
+    public void editarCliente(Cliente cliente, String nome){
+
+        cliente.setNome(nome);
+    }
+
+    public boolean removerCliente(Cliente cliente){
+
+        return Database.getInstance().remover(cliente);
+    }
 }
