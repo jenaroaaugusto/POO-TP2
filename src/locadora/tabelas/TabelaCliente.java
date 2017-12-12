63b9c8a0e9b05bc42fece217479c04d1ef928f73
Maycon-Ferreira <32937928+Maycon-Ferreira@users.noreply.github.com>
2017-12-12 19:38:17 -0200

    Criado GUI princpipal e do cliente

---
+++ src/locadora/tabelas/TabelaCliente.java
@@ -0,0 +1,64 @@
+package locadora.tabelas;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import locadora.modelo.Cliente;
+
+public class TabelaCliente extends AbstractTableModel {
+
+    private List<Cliente> clientes;
+    private String[] colunas = { "Nome", "CPF" };
+    private Class[] classes = { String.class, String.class };
+
+    public TabelaCliente(List<Cliente> clientes){
+        this.clientes = clientes;
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return colunas[i];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int i) {
+        return classes[i];
+    }
+
+    @Override
+    public void setValueAt(Object data, int i, int j) {
+        switch (j) {
+            case 0:
+                clientes.get(i).setNome((String) data);
+            case 1:
+                clientes.get(i).setCpf((String) data);
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int i, int j) {
+        switch (j) {
+        case 0:
+            return clientes.get(i).getNome();
+        case 1:
+            return clientes.get(i).getCpf();
+        default:
+            return null;
+        }
+    }
+
+    @Override
+    public int getRowCount() {
+        return clientes.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    public Cliente getValueAt(int i){
+        return clientes.get(i);
+    }
+}
